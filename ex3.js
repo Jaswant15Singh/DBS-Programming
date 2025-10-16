036a1160be3f77877cf6d98cb246573947037c51
Jaswant Singh <singhjaswant0932@gmail.com>
2025-10-16 11:14:05 +0100

    ex3

--- ex3.js
+++ ex3.js
@@ -15,21 +15,18 @@ let eulerlist = () => {
   a = 2; //can un-hardcode
   b = 3;
   l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10]; //document.getElementById('l').value.split(" ").map((x=>parseInt(x)); // if l is space separated numbers
-  //call listEuler1 and alert. - compute the sum of multiples of a or b in list l
  console.log( listEuler1(a, b, l));
  
 };
 let euler2Lists = () => {
   a = [2, 3]; //can un-hardcode
   l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
-  //call listEuler2 and alert.
   listEuler2(a, l);
 };
 
 let euler2Lists1 = () => {
   a = [2, 3, 5]; //can un-hardcode
   l = [1, 2, 3, 4, 5, 6, 7, 9, 10, 10, 10];
-  //call listEuler3 and alert.
   listEuler3(a, l);
 };
 
